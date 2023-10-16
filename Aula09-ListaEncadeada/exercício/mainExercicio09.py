e69ff1e58254b24b233001edaca2343ab46a86da
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2023-10-16 19:39:45 -0300

    Aula09 - Lista Encadeada - exercício

---
+++ Aula09-ListaEncadeada/exercício/mainExercicio09.py
@@ -0,0 +1,14 @@
+from Lista  import Lista
+
+lista = Lista()
+
+lista.add( 20 )
+lista.add( 10 )
+lista.add( 15 )
+lista.add( 18 )
+lista.add( 5 )
+
+lista.remover( 12 )
+lista.remover( 15 )
+lista.remover( 5 )
+lista.remover( 20 )
