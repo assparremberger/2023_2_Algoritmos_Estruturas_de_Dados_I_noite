c2c4956b7d304d6c8b73a87b0870087a119ce4e8
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2023-10-23 20:32:03 -0300

    Aula10 - 16/10/2023 - Lista Duplamente encadeada

---
+++ Aula10-ListaDuplamenteEncadeada/No.py
@@ -0,0 +1,7 @@
+class No:
+
+    def __init__(self, valor):
+        self.dado = valor
+        self.anterior = None
+        self.proximo = None
+        
---
+++ Aula10-ListaDuplamenteEncadeada/main10.py
@@ -0,0 +1,18 @@
+from Lista import Lista
+
+dupla = Lista()
+
+dupla.add( 10 )
+dupla.add( 5 )
+dupla.add( 15 )
+dupla.add( 8 )
+dupla.add( 3 )
+
+dupla.imprimirReverso()
+
+dupla.remover( 20 )
+dupla.remover( 3 )
+dupla.remover( 8 )
+dupla.remover( 15 )
+
+dupla.imprimirReverso()
