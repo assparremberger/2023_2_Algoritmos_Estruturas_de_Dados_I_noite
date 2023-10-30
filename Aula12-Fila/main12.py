336cd4e6ddf8e37243aa560d0faf8c79c689deac
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2023-10-30 19:46:29 -0300

    Aula12 - 23/10/2023 - Fila - FIFO

---
+++ Aula12-Fila/main12.py
@@ -0,0 +1,14 @@
+from Fila import Fila
+
+fila = Fila()
+
+fila.imprimir()
+
+fila.add( "Adalto" )
+fila.add( "Ivonei" )
+fila.add( "Rafael" )
+fila.remover()
+fila.add( "Júlio" )
+fila.remover()
+fila.remover()
+fila.remover()
